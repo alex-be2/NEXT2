f95aa4c56ba7b6a0feb6401e05c7e9ce36412f35
ALEX BEDDUS <c3035237@hallam.shu.ac.uk>
2025-02-17 16:42:30 +0000

    Add project files.

---
+++ NEXT2/Components/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NEXT2.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options) { }
+
+		public DbSet<Question> Questions { get; set; }
+		public DbSet<Answer> Answers { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+
+}
+public class Question
+{
+    public int QuestionID { get; set; }
+    public string QuestionText { get; set; }
+    public int? ParentQuestionID { get; set; }
+    public string Category { get; set; }
+    public List<Answer> Answers { get; set; }
+}
+
+public class Answer
+{
+    public int AnswerID { get; set; }
+    public int QuestionID { get; set; }
+    public string AnswerText { get; set; }
+    public int? NextQuestionID { get; set; }
+}
+
+public class User
+{
+    public int userID { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public string PasswordHash { get; set; }
+}
