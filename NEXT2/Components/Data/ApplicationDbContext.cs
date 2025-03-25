d264c0687e30ad451fadafeb3a9401aef8fa2d3c
ted-beswick <c3016004@hallam.shu.ac.uk>
2025-03-25 13:43:47 +0000

    cahnge url from null to -1

--- NEXT2/Components/Data/ApplicationDbContext.cs
+++ NEXT2/Components/Data/ApplicationDbContext.cs
@@ -103,7 +103,7 @@ public class RoleAnswer
 public class Result
 {
     public int ResultID { get; set; }
-    public int SessionId { get; set; }
+    public int SessionID { get; set; }
     public int? UserID { get; set; }
     public int Role1ID { get; set; }
     public int Role1Score { get; set; }
